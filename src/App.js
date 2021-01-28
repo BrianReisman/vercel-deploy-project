68f0e955b292f699a0c981114be2a02eb17020b8
BrianReisman <brianreisman@protonmail.com>
2021-01-28 13:10:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -14,42 +14,28 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Brian Made this starting in Vercel. Check 'em out!</h1>
       <h2>
-        Deployed with{' '}
+        Brian lives at{" "}
         <a
-          href="https://vercel.com/docs"
+          href="https://IwantedBrian.com"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Vercel
+          This site!
         </a>
         !
       </h2>
       <p>
         <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
+          href="https://github.com/BrianReisman"
           target="_blank"
           rel="noreferrer noopener"
         >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
+          My GitHub
+        </a>{" "}
+        is where you can take a look at just about anything I've made.
       </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
